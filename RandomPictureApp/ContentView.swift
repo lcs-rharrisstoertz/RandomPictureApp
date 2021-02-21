f21c57b3618582e12077d9d74e3e5bd0ebfb34e7
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-21 15:28:08 -0500

    Initial Commit

---
+++ RandomPictureApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomPictureApp
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomPictureApp/RandomPictureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPictureAppApp.swift
+//  RandomPictureApp
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-21.
+//
+
+import SwiftUI
+
+@main
+struct RandomPictureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
